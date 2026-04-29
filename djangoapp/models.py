aa4584f3a564d004f3562d4b4a2a9087d0e1805d
Mustafo <aminboevmustafo5@gmail.com>
2026-04-29 16:41:00 +0500

    crud

--- djangoapp/models.py
+++ djangoapp/models.py
@@ -1,23 +1,23 @@
 from django.db import models
 from django.utils.text import slugify
 import uuid
-# Create your models here.
+
 
 class User(models.Model):
-    ism=models.CharField(max_length=50,blank=False)
-    familya=models.CharField(max_length=50,blank=False)
-    yosh=models.PositiveIntegerField(default=15,blank=True)
-    phon=models.CharField(max_length=15, null=True, blank=True)
-    email=models.EmailField(max_length=100, null=True, blank=True)
-    pitcture=models.ImageField(upload_to='images/',default='images/default.jpg',blank=True)
-    slug=models.SlugField(blank=True,unique=True)
-    
-    
-    def save(self,*args, **kwargs):
+    ism = models.CharField(max_length=50)
+    familya = models.CharField(max_length=50)
+    yosh = models.PositiveIntegerField(default=15)
+    phon = models.CharField(max_length=15, null=True, blank=True)
+    picture = models.ImageField(upload_to='images/', default='images/default.jpg', blank=True)
+
+    slug = models.SlugField(blank=True, unique=True)
+
+    def save(self, *args, **kwargs):
         if not self.slug:
             short_id = str(uuid.uuid4())[:5]
-        self.slug = slugify(f"{self.ism}-{self.familya}-{short_id}")
+            self.slug = slugify(f"{self.ism}-{self.familya}-{short_id}")
+
         super().save(*args, **kwargs)
-    
+
     def __str__(self):
-        return f'{self.ism} {self.familya}' 
+        return f"{self.ism} {self.familya}"
--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.mypro,name='user_list'),
-    path("user/<slug:slug>/", views.user_view, name='user_view')
+    path("users/", views.UserListView.as_view(), name="user_list"),
+    path("create/", views.UserCreate.as_view(), name="user_create"),
+    path('user/<slug:slug>/update/', views.UserUpdate.as_view(), name='user_update'),
+    path('user/<slug:slug>/delete/', views.UserDelete.as_view(), name='user_delete'),
+    path('user/<slug:slug>/', views.UserDetail.as_view(), name='user_view'),
 ]
