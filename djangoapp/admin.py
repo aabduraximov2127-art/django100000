cf7fe4512c05575e868fd7ba265f291ac131ef43
Mustafo <aminboevmustafo5@gmail.com>
2026-05-02 13:00:08 +0500

    admin

--- djangoapp/admin.py
+++ djangoapp/admin.py
@@ -1,4 +1,25 @@
 from django.contrib import admin
-from  . import models
-# Register your models here.
-admin.site.register(models.User)
+from django.utils.html import format_html
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'ism', 'familya', 'yosh', 'email', 'phon', 'image_tag')
+    search_fields = ('ism', 'familya', 'email', 'phon', 'slug')
+    list_filter = ('yosh',)
+    ordering = ('-id',)
+    list_per_page = 10
+
+    readonly_fields = ('slug',)
+
+    def image_tag(self, obj):
+        if obj.picture:
+            return format_html(
+                '<img src="{}" width="40" style="border-radius:50%;" />',
+                obj.picture.url
+            )
+        return "Rasm topilmadi"
+
+    image_tag.short_description = "Rasm"
--- djangoapp/models.py
+++ djangoapp/models.py
@@ -4,19 +4,19 @@ import uuid
 
 
 class User(models.Model):
-    ism=models.CharField(max_length=50)
-    familya=models.CharField(max_length=50)
-    yosh=models.PositiveIntegerField(default=15)
-    email=models.EmailField(null=True, blank=True)
-    phon=models.CharField(max_length=15, null=True, blank=True)
-    picture=models.ImageField(upload_to='images/', default='images/default.jpg', blank=True)
+    ism = models.CharField(max_length=50)
+    familya = models.CharField(max_length=50)
+    yosh = models.PositiveIntegerField(default=15)
+    email = models.EmailField(null=True, blank=True)
+    phon = models.CharField(max_length=15, null=True, blank=True)
+    picture = models.ImageField(upload_to='images/', default='images/default.jpg', blank=True)
 
     slug = models.SlugField(blank=True, unique=True)
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            slug = str(uuid.uuid4())[:5]
-            self.slug = slugify(f"{self.ism}-{self.familya}-{slug}")
+            uid = str(uuid.uuid4())[:5]
+            self.slug = slugify(f"{self.ism}-{self.familya}-{uid}")
 
         super().save(*args, **kwargs)
 
